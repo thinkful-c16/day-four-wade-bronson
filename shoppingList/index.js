b0bacc1d84df3b58eabef956b9330f39bb6e38fc
Bronson <keliiakimoto@gmail.com>
2017-11-02 13:42:57 -0700

    submit text

--- shoppingList/index.js
+++ shoppingList/index.js
@@ -19,3 +19,12 @@ function deleteItem() {
 }
 
 $(deleteItem);
+
+function addItem() {
+  const addItemInput = $('#js-shopping-list-form').find('input').text();
+  $('#js-shopping-list-form').on('submit', function(event) {
+    $('ul').append('<li><span class="shopping-item"> ' + $(addItemInput) + ' </span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+  });
+}
+
+$(addItem);
